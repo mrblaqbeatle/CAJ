9560e0ac8998aa78a56bc4e38c50ad2b2605000d
mrblaqbeatle <ezraalpha787@gmail.com>
2025-05-21 20:10:29 +0300

    Attempt to fix admin.html auth with same config

--- js/auth-guard.js
+++ js/auth-guard.js
@@ -1,14 +1,13 @@
-// Firebase config
-const firebaseConfig = {
-  apiKey: "AIzaSyDcxwqjoFEUYKTwiuQKMkAfG9y9HZFPCVg",
-  authDomain: "smau-256.firebaseapp.com",
-  databaseURL: "https://smau-256-default-rtdb.firebaseio.com",
-  projectId: "smau-256",
-  storageBucket: "smau-256.appspot.com",
-  messagingSenderId: "76824679086",
-  appId: "1:76824679086:web:c15949b85fa59e9dc3533f"
+// js/firebase-config.js
+var firebaseConfig = {
+  apiKey: "AIzaSyCRR-dm2pTkb09WNalKrrDCD0HFgHHH0W4",
+  authDomain: "caj-website-256.firebaseapp.com",
+  projectId: "caj-website-256",
+  storageBucket: "caj-website-256.firebasestorage.app",
+  messagingSenderId: "138259158609",
+  appId: "1:138259158609:web:483bbdc82dd5abca884a70",
+  measurementId: "G-DHQCJ2C0FB"
 };
-
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
